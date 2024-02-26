506ee42e0318bc9d3ed2a051d6d816e42ad8a2f7
Destiny <155449292+DESTAMMM@users.noreply.github.com>
2024-02-26 18:00:41 +0000

    commit 26/02/24

---
+++ consume.py
@@ -0,0 +1,33 @@
+from zeep import Client      
+from http.server import HTTPServer
+from pysimplesoap.server import SoapDispatcher, SOAPHandler
+
+client = Client(
+    "https://www.dataaccess.com/webservicesserver/NumberConversion.wso?WSDL
+) 
+
+Result = client.serviceNumberToWords(5) 
+print(result)
+
+def saludar(nombre):    
+    return "Hola,{ }!".format(nombre)
+
+dispatcher =SoapDispatcher(
+    "Ejemplo-soap-server",  
+    location="http://localhost:8000/",
+    action="http://localhost:8000/",
+    namespace="http://localhost:8000/",
+    trace="http://localhost"
+
+)
+dispatcher.register_function(
+    "Saludar",
+    saludar,
+    returns={"saludo": str}
+    args={"nombre":str}
+)
+
+server=HTTPServer(("0.0.0.0",8000), SOAPHandler)
+server.dispatcher=dispatcher
+print("Server SOAP iniciado en http://localhost:8000/")
+sevre.serve_forever()
